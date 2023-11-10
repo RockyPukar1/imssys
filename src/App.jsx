541aaf58c57c21bb7927529b85fe0fb0c0c906fc
Rocky Pukar <98908559+RockyPukar1@users.noreply.github.com>
2023-11-10 07:42:04 +0000

    feat: Sing up page design

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import { useState } from 'react'
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
 import Login from './views/Login'
+import SignUp from './views/SignUp'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <Login />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/views/SignUp/index.jsx
@@ -0,0 +1,61 @@
+import LogoIcon from "../../assets/LogoIcon"
+import SocialIcon from "../../assets/SocialIcon";
+
+const SignUp = () => {
+  return (
+    <div>
+      <div className="container mx-auto">
+        <div className="cover absolute inset-0 flex items-center justify-center gap-96">
+          <div className="left flex flex-col gap-5">
+            <div className="h-48">
+              <LogoIcon />
+            </div>
+            <div className="uppercase text-[#009ED8] text-5xl">kanban</div>
+          </div>
+          <div className="right">
+            <div className="w-full">
+              <div className="rounded px-8 pt-6 pb-8 mb-4">
+                <form className="bg-white mb-6">
+                  <div className="mb-4">
+                    <label className="block text-gray-700 text-sm mb-2 text-left" htmlFor="username">
+                      Name*
+                    </label>
+                    <input className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none" id="username" type="text" placeholder="Enter you name" />
+                  </div>
+                  <div className="mb-4">
+                    <label className="block text-gray-700 text-sm mb-2 text-left" htmlFor="username">
+                      Email*
+                    </label>
+                    <input className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none" id="username" type="text" placeholder="Enter you email" />
+                  </div>
+                  <div className="mb-4">
+                    <label className="block text-gray-700 text-sm mb-2 text-left" htmlFor="password">
+                      Password*
+                    </label>
+                    <input className="appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none" id="password" type="password" placeholder="Enter you password" />
+                  </div>
+                  <div className="flex items-center justify-between gap-6">
+                    <div className="flex items-center gap-2">
+                      <input className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none" type="checkbox" name="remember" id="remember" />
+                      <label htmlFor="remember" className="text-sm">Remember for 30 days</label>
+                    </div>
+                    <a className="inline-block align-baseline text-sm text-blue-500 hover:text-blue-800 text-primary-600" href="#">
+                      Forgot Password?
+                    </a>
+                  </div>
+                </form>
+                <button className="w-full text-grey-50 bg-primary-600 text-center mb-4 rounded-md p-2">Get Started</button>
+                <button className="w-full text-center mb-4 rounded-md flex gap-3 items-center justify-center p-3 border"><SocialIcon /> <span>Sign up with Google</span></button>
+                <p className="text-center text-gray-500 text-xs">
+                  Already have an account? <span className="text-primary-600">Log up</span>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default SignUp;
